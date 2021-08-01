2b8fc5de5c013dc443096a4976adb96390d924d1
SrikantKanekar <kanekarsrikant01@gmail.com>
2021-08-01 17:32:38 +0530

    css fixes

--- src/components/features/checkout/checkout.jsx
+++ src/components/features/checkout/checkout.jsx
@@ -5,9 +5,9 @@ import account from "../../../services/accountService";
 import checkout from "../../../services/checkoutService";
 import FormContainer from "../../form/formContainer";
 import Form from "../../form/form";
-import Button from "../../util/button/button";
 import Title from "../../util/title/title";
-import "./checkout.module.css"
+import Total from "./total/total";
+import styles from "./checkout.module.css"
 
 class Checkout extends Form {
     state = {
@@ -62,15 +62,6 @@ class Checkout extends Form {
             .label("Pin Code")
     };
 
-    calculateTotal = () => {
-        const objects = this.state.objects
-        let total = 0
-        objects.forEach((object) => {
-            total += object.quantity * object.slicingDetails.totalPrice
-        })
-        return total
-    }
-
     doSubmit = async () => {
         try {
             const {data} = this.state;
@@ -96,80 +87,36 @@ class Checkout extends Form {
         const {objects} = this.state
 
         return (
-            <div className="container">
+            <div className={`container ${styles.checkout}`}>
                 {objects.length && (
                     <div className="row">
                         <div className="col-lg-6">
-                            <div className="checkout_section">
-                                <FormContainer
-                                    title="Billing Address"
-                                    subtitle="Enter your address info"
-                                    formError={this.state.formError}>
-                                    <div className="row">
-                                        <div className="col-xl-6">
-                                            {this.renderInput("firstname", "First Name*")}
-                                        </div>
-                                        <div className="col-xl-6">
-                                            {this.renderInput("lastname", "Last Name*")}
-                                        </div>
+                            <FormContainer
+                                title="Billing Address"
+                                subtitle="Enter your address info"
+                                formError={this.state.formError}>
+                                <div className="row">
+                                    <div className="col-xl-6">
+                                        {this.renderInput("firstname", "First Name*")}
                                     </div>
-                                    {this.renderInput("phoneNumber", "Phone no*", "phone")}
-                                    {this.renderInput("address", "Address*")}
-                                    {this.renderSelect("city", "City/Town*", this.cities)}
-                                    {this.renderSelect("state", "State*", this.states)}
-                                    {this.renderSelect("country", "Country*", this.countries)}
-                                    {this.renderInput("pinCode", "PinCode*", "number")}
-                                </FormContainer>
-                            </div>
+                                    <div className={`col-xl-6 ${styles.margin}`}>
+                                        {this.renderInput("lastname", "Last Name*")}
+                                    </div>
+                                </div>
+                                {this.renderInput("phoneNumber", "Phone no*", "phone")}
+                                {this.renderInput("address", "Address*")}
+                                {this.renderSelect("city", "City/Town*", this.cities)}
+                                {this.renderSelect("state", "State*", this.states)}
+                                {this.renderSelect("country", "Country*", this.countries)}
+                                {this.renderInput("pinCode", "PinCode*", "number")}
+                            </FormContainer>
                         </div>
 
                         <div className="col-lg-6">
-                            <div className="checkout_order checkout_section">
-                                <div className="section_title">Your order</div>
-                                <div className="section_subtitle">Order details</div>
-
-                                <div className="order_list_container">
-                                    <div className="header">
-                                        <div className="name">Object</div>
-                                        <div className="details">
-                                            <div className="quantity">Qty.</div>
-                                            <div className="price">Price</div>
-                                        </div>
-                                    </div>
-                                    <ul className="order_list">
-                                        {objects.map(object =>
-                                            <li className="list_item object">
-                                                <div className="object">{object.name}</div>
-                                                <div className="details">
-                                                    <div className="quantity">X<span>{object.quantity}</span></div>
-                                                    <div className="price">
-                                                        <i className="fa fa-inr"/><span>{object.slicingDetails.totalPrice}</span>
-                                                    </div>
-                                                </div>
-                                            </li>
-                                        )}
-                                        <li className="list_item">
-                                            <div className="object">Subtotal</div>
-                                            <div className="price subtotal">
-                                                <i className="fa fa-inr"/><span>{this.calculateTotal()}</span>
-                                            </div>
-                                        </li>
-                                        <li className="list_item">
-                                            <div className="object">Shipping</div>
-                                            <div className="price">Free</div>
-                                        </li>
-                                        <li className="list_item">
-                                            <div className="object">Total</div>
-                                            <div className="price total">
-                                                <i className="fa fa-inr"/><span>{this.calculateTotal()}</span>
-                                            </div>
-                                        </li>
-                                    </ul>
-                                </div>
-                                <div className="order_button">
-                                    <Button label="Place Order" onClick={this.handleSubmit}/>
-                                </div>
-                            </div>
+                            <Total
+                                objects={objects}
+                                handleSubmit={this.handleSubmit}
+                            />
                         </div>
                     </div>
                 )}
---
+++ src/components/features/checkout/total/total.jsx
@@ -0,0 +1,66 @@
+import React from 'react';
+import Button from "../../../util/button/button";
+import Title from "../../../util/title/title";
+import Subtitle from "../../../util/subtitle/subtitle";
+import styles from "./total.module.css"
+
+function Total(props) {
+    return (
+        <div className={styles.total}>
+            <Title>Your order</Title>
+            <Subtitle>Order details</Subtitle>
+
+            <div className={styles.container}>
+                <div className={styles.header}>
+                    <div className={styles.name}>Object</div>
+                    <div className={styles.details}>
+                        <div>Qty.</div>
+                        <div>Price</div>
+                    </div>
+                </div>
+                <ul className={styles.list}>
+                    {props.objects.map(object =>
+                        <li>
+                            <div className={styles.name}>{object.name}</div>
+                            <div className={styles.details}>
+                                <div>X{object.quantity}</div>
+                                <div>
+                                    <i className="fa fa-inr"/> {object.slicingDetails.totalPrice}
+                                </div>
+                            </div>
+                        </li>
+                    )}
+                    <li>
+                        <div>Subtotal</div>
+                        <div>
+                            <i className="fa fa-inr"/> {calculateTotal(props.objects)}
+                        </div>
+                    </li>
+                    <li>
+                        <div>Shipping</div>
+                        <div>Free</div>
+                    </li>
+                    <li>
+                        <div>Total</div>
+                        <div>
+                            <i className="fa fa-inr"/> {calculateTotal(props.objects)}
+                        </div>
+                    </li>
+                </ul>
+            </div>
+            <div className={styles.button}>
+                <Button label="Place Order" onClick={props.handleSubmit}/>
+            </div>
+        </div>
+    );
+}
+
+function calculateTotal(objects) {
+    let total = 0
+    objects.forEach((object) => {
+        total += object.quantity * object.slicingDetails.totalPrice
+    })
+    return total
+}
+
+export default Total;
